68bc31e551ab3834bcef883b3abf5e06a610f3e7
oliullah05 <mdolihasan@gmail.com>
2024-01-12 18:57:45 +0600

    Project Setup (React + Tailwind + Shadcn)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      My ToDo!
+      <Button>shed button</Button>
+    </h1>
+    </>
+  )
+}
+
+export default App
